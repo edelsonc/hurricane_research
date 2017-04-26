50bb83f61fd80b61eb86c8e6d228c4963e7957f4
edelsonc <Charles.Edelson12@ncf.edu>
2017-04-26 00:16:40 -0400

    Added new thing...you know what I mean...the one with the banana scale

--- prior_class.py
+++ prior_class.py
@@ -6,3 +6,12 @@ created: 04/20/2017
 """
 import numpy
 from scipy.stats import poisson
+from functools import lru_cache
+
+
+if __name__ == "__main__":
+    d = HurricaneDist(12)
+    print(d.posterior(10))
+    d.update(33)
+    print(d.posterior(10))
+    
